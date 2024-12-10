5a3e36d34ac9b6b3552a010a197ea21df52010d0
Shouvik Paul <shouvik.paul@ah.nl>
2024-12-10 10:37:10 +0100

    initial commit

---
+++ src/main/java/com/shvkpaul/employee/EmployeeProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.shvkpaul.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shvkpaul/employee/EmployeeProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shvkpaul.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
