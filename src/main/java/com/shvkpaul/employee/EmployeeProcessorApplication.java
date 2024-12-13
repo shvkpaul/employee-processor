410435760fa1d885468a6481827feac7677ca5eb
Shouvik Paul <shouvik.paul@ah.nl>
2024-12-13 12:30:03 +0100

    added retry mechanism

--- src/main/java/com/shvkpaul/employee/EmployeeProcessorApplication.java
+++ src/main/java/com/shvkpaul/employee/EmployeeProcessorApplication.java
@@ -2,8 +2,10 @@ package com.shvkpaul.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class EmployeeProcessorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/shvkpaul/employee/service/EmployeeService.java
+++ src/main/java/com/shvkpaul/employee/service/EmployeeService.java
@@ -4,12 +4,17 @@ import com.shvkpaul.employee.client.EmployeeClient;
 import com.shvkpaul.employee.client.model.GenericResponse;
 import com.shvkpaul.employee.model.EmployeeRequest;
 import com.shvkpaul.employee.model.EmployeeResponse;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import static com.shvkpaul.employee.mapper.EmployeeMapper.toEmployeeClientRequest;
 import static com.shvkpaul.employee.mapper.EmployeeMapper.toEmployeeResponse;
 
 @Service
+@Log4j2
 public class EmployeeService {
 
     private final EmployeeClient employeeClient;
@@ -30,7 +35,13 @@ public class EmployeeService {
         return toEmployeeResponse(employeeClient.updateEmployee(employeeId, toEmployeeClientRequest(employeeRequest)));
     }
 
+    @Retryable(
+        value = { Exception.class },
+        maxAttempts = 3,
+        backoff = @Backoff(delay = 20000)
+    )
     public GenericResponse deleteEmployee(Long employeeId) {
+        log.info("Deleting employee with id: {}", employeeId);
         return employeeClient.deleteEmployee(employeeId);
     }
 }
